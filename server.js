5e9add9fc6c7217b64961deaebc574b3144ec1f2
carsonlandvatter <landvattercarson@gmail.com>
2026-01-21 20:35:55 -0700

    Added put, post, and delete routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongodb = require('./data/database');
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
